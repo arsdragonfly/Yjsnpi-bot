b1887fbf0a52a60f36a6c94da88b2b3da6539b55
Unknown <arsdragonfly@gmail.com>
2018-09-23 18:38:52 -0400

    Rewrite parts
    replace await with Future

--- bible.js
+++ bible.js
@@ -1,21 +1,28 @@
+//HTTP stuff
 const request = require("superagent");
+
+//type system & promise-like stuff
+const Future = require("fluture");
 const {
   create,
   env
 } = require("sanctuary");
+const {
+  env: flutureEnv
+} = require('fluture-sanctuary-types');
 const $ = require("sanctuary-def");
 const type = require("sanctuary-type-identifiers");
 const S = create({
   checkTypes: process.env.NODE_ENV !== "production",
-  env: env,
+  env: env.concat(flutureEnv),
 });
+
 const tmp = require("tmp-promise");
+
 const spawn = require('child-process-promise').spawn;
 
-//TODO: seperate this into downloading title and generating path;
-const downloadBible = (async () => {
-  let [title, path] = await Promise.all([
-    //title :: Maybe String
+//DownloadBibleTitle:: Future e String
+let downloadBibleTitle = Future.tryP(() =>
     request.get("https://api.imjad.cn/bilibili/v2/").query({
       //“Whoever believes and is baptized will be saved; whoever does not believe will be condemned.”
       //--Matthew 7:17-21
@@ -23,39 +30,33 @@ const downloadBible = (async () => {
       //R.I.P. Sep. 21, 2018
       //aid: 2557
       aid: 32153770
-    }).then(res => S.gets(S.is($.String))(["body", "data", "title"])(res),
-      reason => S.Nothing),
-    //path :: Maybe String
-    tmp.tmpName({
+    }).then(S.gets(S.is($.String))(["body", "data", "title"])))
+  .chain(S.maybe(Future.reject("Failed to parse json from Bilibili API"))(Future.resolve));
+
+//path:: Future e String
+let downloadBible = Future.tryP(() => tmp.tmpName({
       template: "tmp-XXXXXX"
-    }).then(S.Just, reason => S.Nothing)
-  ])
-  let titleCheck = S.flip(S.maybe_(() => {
-    console.error("Cannot connect to Bilibili.");
-    return S.Nothing;
-  }))(title);
-  let pathCheck = S.flip(S.maybe_(() => {
-    console.error("Cannot generate temp pathname.");
-    return S.Nothing;
-  }))(path);
-  return titleCheck((title) =>
-    pathCheck((path) => {
-      //the path is a glob string
-      //TODO: allow choosing target directory
-      let pathPromise = spawn("annie", ["-o", "/tmp/", "-O", path, "av32153770"], {shell: true, timeout: 114514}).then(() => S.Just("/tmp/" + path + ".*")).catch(e => {console.log(e); return S.Nothing});
-      console.log(pathPromise);
-      console.log(pathPromise.childProcess.pid);
-      pathPromise.childProcess.stdout.on('data', function (data) {
-        console.log('[spawn] stdout: ', data.toString());
-      });
-      return S.Just({
-        title: title,
-        path: pathPromise
-      });
     })
-  );
-})
+    .then(S.toMaybe))
+  .chain(S.maybe(Future.reject("Failed to generate temp path name"))(Future.resolve))
+  .chain(Future.encaseP(path => {
+    let pathPromise = spawn("annie",
+      ["-o", "/tmp/", "-O", path, "av32153770"], {
+        shell: true,
+        timeout: 114514
+      }).then(() => "/tmp/" + path + ".*");
+
+    //Some logging
+    console.log(pathPromise);
+    console.log(pathPromise.childProcess.pid);
+    pathPromise.childProcess.stdout.on('data', data => {
+      console.log('[spawn] stdout: ', data.toString());
+    });
+
+    return pathPromise;
+  }));
 
 module.exports = {
+  downloadBibleTitle,
   downloadBible
 };
